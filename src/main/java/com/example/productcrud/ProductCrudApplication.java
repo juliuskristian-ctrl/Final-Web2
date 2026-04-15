6637114af70f0ec211a5eb576145454086eb4bcd
Ellen Gabriela Serumena <2482031@unai.edu>
2026-04-15 09:42:53 +0700

    ellen

--- src/main/java/com/example/productcrud/ProductCrudApplication.java
+++ src/main/java/com/example/productcrud/ProductCrudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductCrudApplication {
-
+// Ellen
     public static void main(String[] args) {
         SpringApplication.run(ProductCrudApplication.class, args);
     }
